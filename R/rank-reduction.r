9a9fbab1f6f0e24818577830f378a7b5b18f528e
Gaston Sanchez <gaston.stat@gmail.com>
2013-11-28 20:28:36 +0100

    add rank reduction

---
+++ R/rank-reduction.r
@@ -0,0 +1,52 @@
+#' @title Wedderburn rank reduction
+#' 
+#' @description Rank reduction of a matrix by means of Wedderburn theorem
+#' 
+#' @details when \code{a = NULL} and-or \code{b = NULL}, they are randomly
+#' generated using \code{rnorm()}
+#' @param X a numeric matrix
+#' @param a optional vector of length equal to the number of rows in \code{X}
+#' @param b optional vector of length equal to the number of columns in \code{X}
+#' @export wedderburn rank_reduction
+#' @aliases wedderburn rank_reduction
+#' @examples
+#' set.seed(9)
+#' X = matrix(runif(12), 4, 3)
+#' 
+#' # wedderburn's rank reduction
+#' wedderburn(X)
+#' 
+#' # synonmym function
+#' rank_reduction(X)
+wedderburn <- function(X, a = NULL, b = NULL) 
+{
+  if (!is_numeric_matrix(X))
+    stop("\n'wedderburn()' requires a numeric matrix")
+  if (is_one_dim(X))
+    stop("\n'X' has rank 1 for 'wedderburn()'")
+  
+  if (!is.null(a)) {
+    if (length(a) != nrow(X))
+      stop("\n'a' and 'X' are incompatible for 'wedderburn()'")
+  } else {
+    a = rnorm(nrow(X))    
+  }
+  
+  if (!is.null(b)) {
+    if (length(b) != ncol(X))
+      stop("\n'b' and 'X' are incompatible for 'wedderburn()'")
+  } else {
+    b = rnorm(ncol(X))    
+  }
+  
+  alpha = as.numeric(t(a) %*% X %*% b)
+  if (abs(alpha) < 1e-15)
+    stop("\n'wedderburn()' failed to reduce rank of 'X'")
+  
+  # rank reduction (deflation)
+  X - (1/alpha) * X %*% b %*% t(a) %*% X
+}
+
+rank_reduction <- function(X, a = NULL, b = NULL) {
+  wedderburn(X, a = a, b = b) 
+}
--- R/rank.r
+++ R/rank.r
@@ -3,26 +3,26 @@
 #' @description returns the rank of a square matrix 
 #' 
 #' @details The rank is calculated based on the selected method
-#' @param x square numeric matrix
+#' @param x numeric matrix
 #' @param method type of decomposition: \code{"qr"} or \code{"chol"}
-#' @export
+#' @export rank_matrix matrix_rank
+#' @aliases rank_matrix matrix_rank
 #' @examples
-#' # a square matrix
 #' set.seed(5)
-#' M1 = matrix(runif(9), 3, 3)
+#' M1 = matrix(runif(21), 3, 7)
 #' 
 #' # rank
 #' rank_matrix(M1)
 #' 
 #' # another matrix
-#' M2 = cbind(c(1,1), c(-2,-2))
+#' M2 = matrix(1:20, 4, 5)
 #' 
 #' # rank
 #' rank_matrix(M2)
 rank_matrix <- function (x, method = "qr")
 {
-  if (!is_square_matrix(x))
-    stop("\n'rank_matrix()' requires a square matrix" )
+  if (!is_numeric_matrix(x))
+    stop("\n'rank_matrix()' requires a numeric matrix" )
   
   if (method == "qr") {
     rank = qr(x)$rank 
@@ -31,4 +31,8 @@ rank_matrix <- function (x, method = "qr")
   }
   # output
   rank
+}
+
+matrix_rank <- function(x, method = "qr") {
+  rank_matrix(x, method = method)
 }
